0f614a144fbe249f8b7f40f6e5d0b4bdfb97c3cf
Cole Ramer <cole.ramer@gmail.com>
2025-07-07 21:03:30 +0000

    print_game and all avail moves

--- lib/main.ml
+++ lib/main.ml
@@ -30,8 +30,35 @@ let non_win =
     ]
 
 let print_game (game : Game.t) =
-  ignore game;
-  print_endline ""
+  let break_line =
+    List.init
+      (Game_kind.board_length game.game_kind
+      + ((Game_kind.board_length game.game_kind - 1) * 3))
+      ~f:(fun _ -> "-")
+    |> String.concat
+  in
+  let row_as_num =
+    List.init (Game_kind.board_length game.game_kind) ~f:(fun i -> i)
+  in
+  let board_as_num = List.map row_as_num ~f:(fun _ -> row_as_num) in
+  let board =
+    List.mapi board_as_num ~f:(fun row_index row_list ->
+        let row_with_piecies =
+          List.mapi row_list ~f:(fun col_index _ ->
+              match
+                Map.find game.board
+                  { Position.row = row_index; column = col_index }
+              with
+              | None -> " "
+              | Some X -> "X"
+              | Some O -> "O")
+        in
+        String.concat ~sep:" | " row_with_piecies)
+  in
+  let board_as_one_string =
+    String.concat ~sep:("\n" ^ break_line ^ "\n") board
+  in
+  print_endline board_as_one_string
 
 let%expect_test "print_win_for_x" =
   print_game win_for_x;
@@ -59,13 +86,35 @@ let%expect_test "print_non_win" =
 
 (* Exercise 1 *)
 let available_moves (game : Game.t) : Position.t list =
-  ignore game;
-  failwith "Implement me!"
+  let list_of_nums_in_board_length =
+    List.init (Game_kind.board_length game.game_kind) ~f:(fun i -> i)
+  in
+  let (all_possible_board_positions : Position.t list) =
+    List.cartesian_product list_of_nums_in_board_length
+      list_of_nums_in_board_length
+    |> List.map ~f:(fun (row, column) -> { Position.row; column })
+  in
+  List.filter all_possible_board_positions ~f:(fun position ->
+      not (Map.mem game.board position))
+
+let%expect_test "availble moves" =
+  print_endline
+    (available_moves win_for_x
+    |> List.map ~f:(fun position -> Position.to_string position)
+    |> String.concat |> String.strip);
+  print_endline
+    (available_moves non_win
+    |> List.map ~f:(fun position -> Position.to_string position)
+    |> String.concat |> String.strip);
+  [%expect
+    {|((row 0) (column 1))((row 0) (column 2))((row 1) (column 1))((row 1) (column 2))((row 2) (column 1))|}];
+  return ()
 
 (* Exercise 2 *)
+
 let evaluate (game : Game.t) : Evaluation.t =
   ignore game;
-  failwith "Implement me!"
+  failwith "todo"
 
 (* Exercise 3 *)
 let winning_moves ~(me : Piece.t) (game : Game.t) : Position.t list =
